7e736f388f0133c1ad6078b0c18f653ef439871f
Pedro <palexb101@gmail.com>
2022-03-25 18:26:11 -0700

    Update and added a blade as welll
    added a blade for an info page

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -3,67 +3,40 @@
 @section('title', 'To Do List')
 
 @section('content_header')
-    <h1>Manufacturers Info</h1>
+    <h1>To Do's</h1>
 @stop
 
-@extends('layouts.app', ['page' => 'New Provider', 'pageSlug' => 'provider', 'section' => 'providers'])
 @section('content')
-    <div class="container-fluid mt--7">
-        <div class="row">
-            <div class="col-xl-12 order-xl-1">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="row align-items-center">
-                            <div class="col-8">
-                                <h3 class="mb-0">New Provider</h3>
-                            </div>
-                            <div class="col-4 text-right">
-                                <a href="{{ route('providers.index') }}" class="btn btn-sm btn-primary">Back to List</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <form method="post" action="{{ route('providers.store') }}" autocomplete="off">
-                            @csrf
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Task</th><th>Progress</th><th style="width: 40px">Action</th>
+        </tr>
+      </thead>
+      <tbody>
 
-                            <h6 class="heading-small text-muted mb-4">Provider Information</h6>
-                            <div class="pl-lg-4">
-                                <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-name">Name</label>
-                                    <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="Name" value="{{ old('name') }}" required autofocus>
-                                    @include('alerts.feedback', ['field' => 'name'])
-                                </div>
+        @foreach($todos AS $todo)
+        <tr>
+          <td>{{ $todo->id }}</td>
+          <td>{{ $todo->title }}</td>
+          <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: {{ $todo->progress }}%"></div></div></td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('todos.show',['todo'=>$todo->id]) }}">View</a></td>
+        </tr>
+        @endforeach
 
-                                <div class="form-group{{ $errors->has('description') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-description">Description</label>
-                                    <input type="text" name="description" id="input-description" class="form-control form-control-alternative{{ $errors->has('description') ? ' is-invalid' : '' }}" placeholder="Description" value="{{ old('description') }}" required>
-                                    @include('alerts.feedback', ['field' => 'description'])
-                                </div>
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('todos.create') }} " class="btn btn-primary" >Create</a>
+@stop
 
-                                <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-email">Email</label>
-                                    <input type="email" name="email" id="input-email" class="form-control form-control-alternative{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Email" value="{{ old('email') }}" required>
-                                    @include('alerts.feedback', ['field' => 'email'])
-                                </div>
-                                <div class="form-group{{ $errors->has('phone') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-phone">Telephone</label>
-                                    <input type="phone" name="phone" id="input-phone" class="form-control form-control-alternative{{ $errors->has('phone') ? ' is-invalid' : '' }}" placeholder="Telephone" value="{{ old('phone') }}" required>
-                                    @include('alerts.feedback', ['field' => 'phone'])
-                                </div>
-                                <div class="form-group{{ $errors->has('paymentinfo') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-paymentinfo">Payment information</label>
-                                    <textarea name="paymentinfo" id="input-paymentinfo" class="form-control form-control-alternative{{ $errors->has('paymentinfo') ? ' is-invalid' : '' }}" placeholder="Payment information" value="{{ old('paymentinfo') }}" required></textarea>
-                                    @include('alerts.feedback', ['field' => 'paymentinfo'])
-                                </div>
-
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-success mt-4">Save</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+@section('js')
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
+@stop
