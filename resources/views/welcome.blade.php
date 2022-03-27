887ace9bb2a7fc74fb322d2d2202b47701b8401c
Pedro <palexb101@gmail.com>
2022-03-26 19:53:15 -0700

    Update welcome.blade.php
    update take away the red

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
   <!-- Header -->
   <div class="w3-container" style="margin-top:80px" id="showcase">
     <h1 class="w3-jumbo"><b>Home page</b></h1>
-    <hr style="width:50px;border:5px solid red" class="w3-round">
+    
   </div>
 
   <!-- Services -->
