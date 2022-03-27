f5d571c08f39a10f33742a539347f77493783db4
Pedro <palexb101@gmail.com>
2022-03-26 21:26:25 -0700

    Update welcome.blade.php
    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,9 +29,7 @@
     <h1 class="w3-xxxlarge w3-text-red"><b>Sales contact information.</b></h1>
 
     <p>We are a Electronic Inc. service that focus on what's best for any electronic equiment and what's best for you!</p>
-    <p>Some text about our services - what we do and what we offer. We are lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor
-    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    <p>  Here put the sales contact and information. 
     </p>
   </div>
 
@@ -41,9 +39,7 @@
     <h1 class="w3-xxxlarge w3-text-red"><b>Tech support contact information</b></h1>
 
     <p>We are a interior design service that focus on what's best for your home and what's best for you!</p>
-    <p>Some text about our services - what we do and what we offer. We are lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor
-    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    <p>Here put the tech support phone number and information
     </p>
   </div>
 
