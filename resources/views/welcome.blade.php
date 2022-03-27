d68e7554c92617ed4136a07d8eec71a3c4ee076a
Pedro <palexb101@gmail.com>
2022-03-26 20:05:58 -0700

    Update welcome.blade.php
    update the comments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,10 +24,10 @@
 
   </div>
 
-  <!-- Services -->
+  <!-- Sales contact Information -->
   <div class="w3-container" id="services" style="margin-top:75px">
     <h1 class="w3-xxxlarge w3-text-red"><b>Sales contact information.</b></h1>
-    
+
     <p>We are a Electronic Inc. service that focus on what's best for any electronic equiment and what's best for you!</p>
     <p>Some text about our services - what we do and what we offer. We are lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
     dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor
