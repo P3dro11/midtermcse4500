1fa0bc2f385398d563c78f15cbe0007ddbd0b395
Pedro <palexb101@gmail.com>
2022-03-28 19:05:27 -0700

    update on my blade pages html

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -8,17 +8,41 @@
 
 @section('content')
 <div class="w3-container w3-card w3-white w3-round w3-margin"><br>
-  <span class="w3-right w3-opacity">1 min</span>
-  <h4>John Doe</h4><br>
+  <h4>Capacitors</h4><br>
   <hr class="w3-clear">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <p>A capacitor is an electronic component characterized by its capacity to store an electric charge. A capacitor is a passive electrical component that can store energy in the electric field between a pair of conductors (called “plates”).
+     Starting price: 5.99 plus taxes
+  </p>
     <div class="w3-row-padding" style="margin:0 -16px">
     </div>
   </div>
   <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fa fa-thumbs-up"></i>  Like</button> 
   <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-comment"></i>  Comment</button> 
 </div>
-
+<div class="w3-container w3-card w3-white w3-round w3-margin"><br>
+  <h4>Integrated Circuit</h4><br>
+  <hr class="w3-clear">
+  <p>Integrated Circuit or IC is an SMD Electronic Component made up of combination of several transistors, diode, resistor, capacitors in a tiny semiconductor chip. Integrated Circuit Electronic Components or IC are of small size and very light weight. They produce excellent results at low power.
+     Starting price: 9.99 plus taxes
+  </p>
+    <div class="w3-row-padding" style="margin:0 -16px">
+    </div>
+  </div>
+  <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fa fa-thumbs-up"></i>  Like</button> 
+  <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-comment"></i>  Comment</button> 
+</div>
+<div class="w3-container w3-card w3-white w3-round w3-margin"><br>
+  <h4>Resistor</h4><br>
+  <hr class="w3-clear">
+  <p>A Resistor is an electrical device that resists the flow of electrical current. It is a passive device used to control, or impede the flow of, electric current in an electric circuit by providing resistance, thereby developing a drop in voltage across the device.
+     Starting price: 2.99 plus taxes
+  </p>
+    <div class="w3-row-padding" style="margin:0 -16px">
+    </div>
+  </div>
+  <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fa fa-thumbs-up"></i>  Like</button> 
+  <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-comment"></i>  Comment</button> 
+</div>
 
 @stop
 
