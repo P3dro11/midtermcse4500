82bada2b69e2cff7bc90da4e5f4992a6b72bf923
Pedro <palexb101@gmail.com>
2022-03-30 16:09:23 -0700

    update my welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,48 +48,7 @@
       <p><i class="fa fa-envelope-o w3-text-teal w3-xlarge"></i>  test@test.com</p>
     </p>
   </div>
-
- <!-- Construct the box with style you want. Here we are using box-danger -->
-      <!-- Then add the class direct-chat and choose the direct-chat-* contexual class -->
-      <!-- The contextual class should match the box, so we are using direct-chat-danger -->
-      <div class="box box-danger direct-chat direct-chat-danger">
-        <div class="box-header with-border">
-          <h3 class="box-title">Direct Chat</h3>
-          <div class="box-tools pull-right">
-            <span data-toggle="tooltip" title="3 New Messages" class="badge bg-red">3</span>
-            <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-            <!-- In box-tools add this button if you intend to use the contacts pane -->
-            <button class="btn btn-box-tool" data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle"><i class="fa fa-comments"></i></button>
-            <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-          </div>
-        </div><!-- /.box-header -->
-        <div class="box-body">
-          <!-- Conversations are loaded here -->
-          <div class="direct-chat-messages">
-            <!-- Message. Default to the left -->
-            <div class="direct-chat-msg">
-              <div class="direct-chat-info clearfix">
-                <span class="direct-chat-name pull-left">Alexander Pierce</span>
-                <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
-              </div><!-- /.direct-chat-info -->
-              <!-- /.direct-chat-img -->
-              <div class="direct-chat-text">
-                Hello, welcome to Electronic Inc.
-              </div><!-- /.direct-chat-text -->
-            </div><!-- /.direct-chat-msg -->
-
-            <!-- Message to the right -->
-            <div class="direct-chat-msg right">
-              <div class="direct-chat-info clearfix">
-                <span class="direct-chat-name pull-right">Sarah Bullock</span>
-                <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
-              </div><!-- /.direct-chat-info -->
-              <!-- /.direct-chat-img -->
-              <div class="direct-chat-text">
-                You better believe it!
-              </div><!-- /.direct-chat-text -->
-            </div><!-- /.direct-chat-msg -->
-          </div><!--/.direct-chat-messages-->
+         
 
 @stop
 
