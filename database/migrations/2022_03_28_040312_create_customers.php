4e2c66570b878276043b644a996904964dd79573
Pedro <palexb101@gmail.com>
2022-03-30 13:37:34 -0700

    update the files for my midterm

--- database/migrations/2022_03_28_040312_create_customers.php
+++ database/migrations/2022_03_28_040312_create_customers.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name');
-            $table->string('number');
+            $table->string('fullname');
             $table->string('email');
+            $table->string('phonenumber');
         });
     }
 
@@ -31,4 +31,3 @@ return new class extends Migration
     {
         Schema::dropIfExists('customers');
     }
-};
