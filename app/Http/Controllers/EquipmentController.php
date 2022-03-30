f7334fb643ef314dd9acb832f1f4f3987aef556d
Pedro <palexb101@gmail.com>
2022-03-30 11:00:09 -0700

    update the files i added

---
+++ app/Http/Controllers/EquipmentController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $todos = Todo::all();
+        return view('todos',compact('todos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('todos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validated = $request->validate([
+            'number' => 'required',
+            'hello' => 'required',
+            'contactinfo' => 'required',
+            'quanity' => 'required',
+            'servicesoftware' => 'required',
+            'price' => 'required',
+            'purchasedate' => 'required',
+       ]);
+
+       $todo = Todo::create([
+            'number' => $request->number,
+            'hello' => $request->name,
+            'contactinfo' => $request->contactinfo,
+            'quanity' => $request->quanity,
+            'servicesoftware' => $request->servicesoftware,
+            'price' => $request->price,
+            'purchasedate' => $request->purchasedate,
+       ]);
+
+       return $this->index();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $todo= Todo::find($id);
+        return view('todos.show',compact('todo'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
