a311bd48bcc6d5c258be270fbae11f5711532d7d
Pedro <palexb101@gmail.com>
2022-03-30 19:01:18 -0700

    update

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Invoice;
-use App\Models\Equipment;
+
 
 use Kris\LaravelFormBuilder\FormBuilder;
 use App\Forms\InvoiceForm;
